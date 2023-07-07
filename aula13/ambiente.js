37c5739be604c346d0b117fde2d7ea92d0b55f11
Gabriel GB <105217214+GabrielGBcode@users.noreply.github.com>
2023-07-07 10:21:53 -0300

    Exercícios 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+var count = 1;
+
+do {
+  console.log(`Olá, tudo bem? ${count}`);
+  count++;
+} while (count <= 6);
+
+// while (count <= 6) {
+//   console.log(`Olá, tudo bem? ${count}`);
+//   count++;
+// }
+
+// array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < array.length; i++) {
+//   const element = array[i];
+//   console.log(element);
+// }
---
+++ aula14/ambiente.js
@@ -0,0 +1,3 @@
+for (let c = 1; c <= 5; c++) {
+  console.log(c);
+}
